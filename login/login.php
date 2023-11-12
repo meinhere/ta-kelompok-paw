7d93c0a9286265db7f72249c55a8b9bfbb1c185d
Syaiful Rochmandani <syaifulrochmandani737@gmail.com>
2023-11-12 20:22:46 +0700

    login register dan daftar pesanan dan detail pesanan

---
+++ login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login PHP</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="main-login">
+            <h2>Login</h2>
+            <form action="#" method="post">
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username">
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+                <a href="test.php"><button>Login</button></a>
+                <p class="login">Belum punya akun? <span><a href="register.php">register</a></span></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
