03d71df90b46337127b4d6e40b7dedd5497feaa3
unknown <rizkyandwip@gmail.com>
2023-11-13 12:12:26 +0700

    edit profil

---
+++ profil/script.js
@@ -0,0 +1,7 @@
+function konfirmasi(){
+    if(window.confirm("Apakah anda sudah yakin?")){
+        window.alert("Perubahan telah disimpan");
+    }else{
+        window.alert("Perubahan dibatalkan")
+    }
+}
